306bc29d5369669a026c166c9a517b67fb19e5ad
Yo wei Hu <j821220j821220@gmail.com>
2019-06-25 20:31:41 +0800

    #258 Another Solution

--- 258_addDigits.js
+++ 258_addDigits.js
@@ -10,3 +10,14 @@ var addDigits = function(num) {
     }
     return num;
 };
+
+// Another Solution
+var addDigits = function(num) {
+    if (num == 0) {
+        return 0;
+    } else if (num % 9 == 0) {
+        return 9;
+    } else {
+        return num % 9;
+    }
+};
